b28b70a53db528175098a8787e1ce687da810911
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2024-06-25 23:15:30 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic7-angular-sqlite-app',
+  webDir: 'www'
+};
+
+export default config;
